2ab7929a140a85446f2719d3f58e5d647b10c919
akioweh <0@akioweh.com>
2025-07-11 18:06:04 +0400

    use indent-blankline for indent guides

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    opts = function(_, opts)
+      opts.indent = {
+        char = "▏",
+        tab_char = "▏",
+      }
+    end,
+  },
+}

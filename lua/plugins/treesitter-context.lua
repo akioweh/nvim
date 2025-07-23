0959ed6ce128277a99bc6d97c9078e8ef0fe89e5
akioweh <0@akioweh.com>
2025-07-23 14:28:09 +0400

    add border to treesitter contect

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = function(_, opts)
+      vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "Grey" })
+      vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { underline = true, sp = "Grey" })
+    end,
+  },
+}

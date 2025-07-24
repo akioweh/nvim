59e127117da97ac76b2d329e33757b90258ba598
akioweh <0@akioweh.com>
2025-07-25 00:31:23 +0400

    patch cmake-tools shell/os behavior some things still are broken but idk

---
+++ lua/plugins/cmake-tools.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "Civitasv/cmake-tools.nvim",
+    config = function(_, opts)
+      require("cmake-tools").setup(opts)
+      local osys = require("cmake-tools.osys")
+      osys.islinux = true
+      osys.iswin32 = false
+    end,
+  },
+}

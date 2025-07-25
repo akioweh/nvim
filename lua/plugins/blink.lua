525ce6b354f2d752c31a02d114df8b9ec4cd5081
akioweh <0@akioweh.com>
2025-07-25 17:39:30 +0400

    fix (clean) up blink.cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,43 +1,90 @@
+local LazyVim = require("lazyvim.util")
+
 return {
-  "saghen/blink.cmp",
-  opts = {
-    appearance = {
-      nerd_font_variant = "normal",
-    },
-    -- the present doesn't work for some reason, so we just copy its contents below
-    -- keymap = {
-    --   preset = "super-tab",
-    -- },
-    keymap = {
-      preset = "none",
-      ["<Tab>"] = {
-        function(cmp)
-          if cmp.snippet_active() then
-            return cmp.accept()
-          else
-            return cmp.select_and_accept()
-          end
+  {
+    "saghen/blink.cmp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    opts = function(_, opts)
+      opts.appearance = opts.appearance or {}
+      opts.appearance.nerd_font_variant = "normal"
+      -- override lazyvim stuff
+      opts.keymap = {
+        preset = "super-tab",
+      }
+      opts.completion = opts.completion or {}
+      opts.completion.list = opts.completion.list or {}
+      opts.completion.list.selection = {
+        auto_insert = true,
+        preselect = function(ctx)
+          return not require("blink.cmp").snippet_active({ direction = 1 })
         end,
-        "snippet_forward",
-        "fallback",
-      },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
-      ["<CR>"] = false,
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-e>"] = { "hide" },
-      -- this is really good with my keyboard nav layer setup
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<Down>"] = { "select_next", "fallback" },
-    },
-    completion = {
-      list = {
-        selection = {
-          auto_insert = true,
-          preselect = function(ctx)
-            return not require("blink.cmp").snippet_active({ direction = 1 })
-          end,
-        },
-      },
-    },
+      }
+      opts.signature = { enabled = true }
+    end,
+    config = function(_, opts)
+      -- setup compat sources
+      local enabled = opts.sources.default
+      for _, source in ipairs(opts.sources.compat or {}) do
+        opts.sources.providers[source] = vim.tbl_deep_extend(
+          "force",
+          { name = source, module = "blink.compat.source" },
+          opts.sources.providers[source] or {}
+        )
+        if type(enabled) == "table" and not vim.tbl_contains(enabled, source) then
+          table.insert(enabled, source)
+        end
+      end
+
+      -- add ai_accept to <Tab> key
+      if not opts.keymap["<Tab>"] then
+        if opts.keymap.preset == "super-tab" then -- super-tab
+          opts.keymap["<Tab>"] = {
+            -- lazyvim here tries indexing "super-tab" instead of using .get()
+            require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+            LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+            "fallback",
+          }
+        else -- other presets
+          opts.keymap["<Tab>"] = {
+            LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+            "fallback",
+          }
+        end
+      end
+
+      -- Unset custom prop to pass blink.cmp validation
+      opts.sources.compat = nil
+
+      -- check if we need to override symbol kinds
+      for _, provider in pairs(opts.sources.providers or {}) do
+        ---@cast provider blink.cmp.SourceProviderConfig|{kind?:string}
+        if provider.kind then
+          local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+          local kind_idx = #CompletionItemKind + 1
+
+          CompletionItemKind[kind_idx] = provider.kind
+          ---@diagnostic disable-next-line: no-unknown
+          CompletionItemKind[provider.kind] = kind_idx
+
+          ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
+          local transform_items = provider.transform_items
+          ---@param ctx blink.cmp.Context
+          ---@param items blink.cmp.CompletionItem[]
+          provider.transform_items = function(ctx, items)
+            items = transform_items and transform_items(ctx, items) or items
+            for _, item in ipairs(items) do
+              item.kind = kind_idx or item.kind
+              item.kind_icon = LazyVim.config.icons.kinds[item.kind_name] or item.kind_icon or nil
+            end
+            return items
+          end
+
+          -- Unset custom prop to pass blink.cmp validation
+          provider.kind = nil
+        end
+      end
+
+      require("blink.cmp").setup(opts)
+    end,
   },
 }

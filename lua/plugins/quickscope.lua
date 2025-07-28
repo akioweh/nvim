b5af02e68e7fc506b5556a8acd6ea827308650a0
akioweh <0@akioweh.com>
2025-07-28 22:11:37 +0400

    blacklist quickscope on non editor buffers

--- lua/plugins/quickscope.lua
+++ lua/plugins/quickscope.lua
@@ -1,5 +1,9 @@
 return {
   {
     "unblevable/quick-scope",
+    init = function()
+      vim.g.qs_buftype_blacklist = { "terminal", "nofile" }
+      vim.g.qs_filetype_blacklist = { "dashboard", "NvimTree", "neo-tree", "Trouble", "help", "alpha" }
+    end,
   },
 }

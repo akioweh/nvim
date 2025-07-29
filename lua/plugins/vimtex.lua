a34eaf49185213c583b75a812ca1ae28c3790cd6
akioweh <0@akioweh.com>
2025-07-29 20:57:41 +0400

    lazyvim already does not lazy load

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,11 +1,10 @@
 return {
   {
     "lervag/vimtex",
-    -- lazy = false, -- disable lazy-loading for proper inverse search support (fr?)
     config = function()
       --   vim.g.vimtex_view_general_viewer = "SumatraPDF"
       --   vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
-      --
+
       vim.g.vimtex_compiler_latexmk = {
         aux_dir = "../auxil", -- 'aux' is a reserved device name on Windows
         out_dir = "../out",

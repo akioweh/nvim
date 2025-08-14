273b565c9f3d7d1e865d0c3b87188b52498b8602
akioweh <0@akioweh.com>
2025-08-14 19:58:15 +0400

    lazy load unused colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,8 @@
 return {
-  { "tiagovla/tokyodark.nvim" },
-  { "bluz71/vim-moonfly-colors", name = "moonfly" },
-  { "xiantang/darcula-dark.nvim" },
-  {
-    "olimorris/onedarkpro.nvim",
-    -- lazy = false,
-    -- priority = 1000,
-  },
+  { "tiagovla/tokyodark.nvim", lazy = true },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = true },
+  { "xiantang/darcula-dark.nvim", lazy = true },
+  { "olimorris/onedarkpro.nvim", lazy = true },
   {
     "catppuccin/nvim",
     name = "catppuccin",

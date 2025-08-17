be5c3b2022918f5608491d09903334fd32b925c1
akioweh <0@akioweh.com>
2025-08-17 23:35:31 +0400

    we don't need this

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       animate = { enabled = false }, -- how dare you try animate a TUI >:(
       scroll = { enabled = false }, -- smooth scrolling
+      dashboard = { enabled = false },
     },
   },
 }

8a05b21069dedeee7bde8ad33a8f8329087b569e
akioweh <0@akioweh.com>
2025-11-19 22:14:32 +0000

    remove background color from treesitter context lines

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -2,7 +2,8 @@ return {
   {
     "nvim-treesitter/nvim-treesitter-context",
     opts = function(_, opts)
-      vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "Grey" })
+      vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "none" })
+      vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = true, sp = "Grey", bg = "none" })
       vim.api.nvim_set_hl(0, "TreesitterContextLineNumberBottom", { underline = true, sp = "Grey" })
     end,
   },

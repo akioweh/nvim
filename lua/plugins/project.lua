bc6ba457fa496570dd66063c745032b48394a35d
akioweh <0@akioweh.com>
2025-12-15 21:50:39 +0400

    add project.nvim

---
+++ lua/plugins/project.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "DrKJeff16/project.nvim",
+    init = function()
+      LazyVim.on_load("telescope.nvim", function()
+        require("telescope").load_extension("projects")
+      end)
+    end,
+    cmd = {
+      "Project",
+      "ProjectAdd",
+      "ProjectConfig",
+      "ProjectDelete",
+      "ProjectHistory",
+      "ProjectRecents",
+      "ProjectRoot",
+      "ProjectSession",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {},
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    optional = true,
+    keys = {
+      {
+        "<leader>fp",
+        function()
+          vim.cmd("Telescope projects")
+        end,
+        desc = "Projects",
+      },
+    },
+  },
+}

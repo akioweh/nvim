f4305c90acefad27023186304bc7fa642d76cc8f
akioweh <0@akioweh.com>
2025-12-16 14:36:08 +0400

    add treesj

---
+++ lua/plugins/treesj.lua
@@ -0,0 +1,32 @@
+return {
+  {
+    "Wansmer/treesj",
+    keys = {
+      {
+        "<space>m",
+        function()
+          require("treesj").toggle()
+        end,
+        mode = { "n", "x" },
+      },
+      {
+        "<space>j",
+        function()
+          require("treesj").join()
+        end,
+        mode = { "n", "x" },
+      },
+      {
+        "<space>J",
+        function()
+          require("treesj").split()
+        end,
+        mode = { "n", "x" },
+      },
+    },
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    opts = {
+      use_default_keymaps = false,
+    },
+  },
+}

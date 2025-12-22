ae29a2eb864fabd0e20cab41b61d3d8dd800ba18
akioweh <0@akioweh.com>
2025-12-23 01:29:16 +0400

    clean up smart-splits keymaps
    turns out it is super-duper-lazy so we can directly require it

--- lua/plugins/smart-splits.lua
+++ lua/plugins/smart-splits.lua
@@ -4,63 +4,26 @@ return {
     "mrjones2014/smart-splits.nvim",
     lazy = false, -- self-lazy
     keys = {
-      {
-        "<C-Up>",
-        function()
-          require("smart-splits").resize_up()
-        end,
-        desc = "Resize Window Up",
-      },
-      {
-        "<C-Down>",
-        function()
-          require("smart-splits").resize_down()
-        end,
-        desc = "Resize Window Down",
-      },
-      {
-        "<C-Left>",
-        function()
-          require("smart-splits").resize_left()
-        end,
-        desc = "Resize Window Left",
-      },
-      {
-        "<C-Right>",
-        function()
-          require("smart-splits").resize_right()
-        end,
-        desc = "Resize Window Right",
-      },
-      {
-        "<leader>wj",
-        function()
-          require("smart-splits").move_cursor_left()
-        end,
-        desc = "Move to Left Window",
-      },
-      {
-        "<leader>wk",
-        function()
-          require("smart-splits").move_cursor_up()
-        end,
-        desc = "Move to Upper Window",
-      },
-      {
-        "<leader>wl",
-        function()
-          require("smart-splits").move_cursor_right()
-        end,
-        desc = "Move to Right Window",
-      },
-      {
-        "<leader>w;",
-        function()
-          require("smart-splits").move_cursor_down()
-        end,
-        desc = "Move to Lower Window",
-      },
+      { "<C-Up>" },
+      { "<C-Down>" },
+      { "<C-Left>" },
+      { "<C-Right>" },
+      { "<leader>wj" },
+      { "<leader>wk" },
+      { "<leader>wl" },
+      { "<leader>w;" },
     },
     opts = {},
+    config = function(_, opts)
+      require("smart-splits").setup(opts)
+      vim.keymap.set("n", "<C-Up>", require("smart-splits").resize_up, { desc = "Resize Window Up" })
+      vim.keymap.set("n", "<C-Down>", require("smart-splits").resize_down, { desc = "Resize Window Down" })
+      vim.keymap.set("n", "<C-Left>", require("smart-splits").resize_left, { desc = "Resize Window Left" })
+      vim.keymap.set("n", "<C-Right>", require("smart-splits").resize_right, { desc = "Resize Window Right" })
+      vim.keymap.set("n", "<leader>wj", require("smart-splits").move_cursor_left, { desc = "Move to Left Window" })
+      vim.keymap.set("n", "<leader>wk", require("smart-splits").move_cursor_up, { desc = "Move to Upper Window" })
+      vim.keymap.set("n", "<leader>wl", require("smart-splits").move_cursor_right, { desc = "Move to Right Window" })
+      vim.keymap.set("n", "<leader>w;", require("smart-splits").move_cursor_down, { desc = "Move to Lower Window" })
+    end,
   },
 }

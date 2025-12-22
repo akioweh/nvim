eb192831d139378d5f3f09e227d625e7146006db
akioweh <0@akioweh.com>
2025-12-23 01:35:44 +0400

    add structural replace

---
+++ lua/plugins/ssr.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "cshuaimin/ssr.nvim",
+    lazy = false, -- self-lazy (config does not load)
+    keys = {
+      {
+        "<leader>sf",
+        function()
+          require("ssr").open()
+        end,
+        desc = "Structural Search and Replace",
+      },
+    },
+    opts = {},
+  },
+}

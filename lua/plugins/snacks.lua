d2b17f66ed0217013ba044dbbd53fac98ea3b8ef
akioweh <0@akioweh.com>
2025-12-23 12:11:43 +0400

    use snacks-picker
    it has better colors :)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,7 +1,12 @@
+if false then
+  require("snacks")
+end
+
 ---@type LazySpec
 return {
   {
     "folke/snacks.nvim",
+    ---@type snacks.Config
     opts = {
       animate = { enabled = false }, -- how dare you try animate a TUI >:(
       scroll = { enabled = false }, -- smooth scrolling

8a1526005da8afc12ec93f79a736fe39e71f36af
akioweh <0@akioweh.com>
2025-12-25 16:34:00 +0400

    add codesettings plugin
    allows per-project lsp server config

---
+++ lua/plugins/codesettings.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "mrjones2014/codesettings.nvim",
+    ft = { "json", "jsonc", "lua" },
+    opts = {
+      config_file_paths = {
+        ".vscode/settings.json",
+        "codesettings.json",
+        "lspsettings.json",
+        ".lspconfig.json",
+      },
+      jsonls_integration = true,
+      lua_ls_integration = true,
+      jsonc_filetype = true,
+      live_reload = true,
+      root_dir = require("lazyvim.util").root(),
+      merge_lists = "append",
+    },
+  },
+}

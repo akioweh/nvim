4018e710b5726a465a061f99cc5b380388004de8
akioweh <0@akioweh.com>
2025-12-25 23:08:22 +0400

    none-ls disable vacuum for now
    (it errors)

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -18,7 +18,7 @@ return {
         nls.builtins.diagnostics.dotenv_linter,
         -- nls.builtins.diagnostics.editorconfig_checker,
         -- nls.builtins.diagnostics.proselint.with({ filetypes = { "tex", "markdown", "typst" } }),
-        nls.builtins.diagnostics.vacuum,
+        -- nls.builtins.diagnostics.vacuum,
         -- nls.builtins.diagnostics.vale.with({ filetypes = { "tex", "markdown", "asciidoc", "typst" } }),
         nls.builtins.hover.dictionary,
         nls.builtins.hover.printenv,

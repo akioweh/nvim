37369674e859a0f9065f8cef1848553e31e6d0c3
akioweh <0@akioweh.com>
2026-01-26 22:48:33 +0000

    adjust none-ls
    enable english word completions only in appropriate filetypes
    also disable vacuum because it keeps erroring

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,9 @@ return {
       opts.sources = vim.list_extend(opts.sources or {}, {
         -- nls.builtins.code_actions.proselint.with({ filetypes = { "tex", "markdown", "typst" } }),
         -- nls.builtins.code_actions.textlint,
-        nls.builtins.completion.spell,
+        nls.builtins.completion.spell.with({
+          filetypes = { "markdown", "tex", "typst", "text", "gitcommit", "gitrebase" },
+        }),
         nls.builtins.completion.tags,
         nls.builtins.diagnostics.actionlint,
         -- nls.builtins.diagnostics.alex,
@@ -17,7 +19,7 @@ return {
         nls.builtins.diagnostics.dotenv_linter,
         -- nls.builtins.diagnostics.editorconfig_checker,
         -- nls.builtins.diagnostics.proselint.with({ filetypes = { "tex", "markdown", "typst" } }),
-        -- nls.builtins.diagnostics.vacuum,
+        nls.builtins.diagnostics.vacuum,
         -- nls.builtins.diagnostics.vale.with({ filetypes = { "tex", "markdown", "asciidoc", "typst" } }),
         nls.builtins.hover.dictionary,
         nls.builtins.hover.printenv,

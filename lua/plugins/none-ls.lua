a376306c8a1dc9358eed0c7448022a0a8a53b524
akioweh <0@akioweh.com>
2026-01-30 04:48:20 +0000

    remove formatters from none-ls
    also mark it optional as it is enabled as lazyvim extra
    
    removed all the default sources (except fish diagnostics) as they're all
    formatters, and we're happy with using conform :)

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,9 +2,12 @@
 return {
   {
     "nvimtools/none-ls.nvim",
+    optional = true,
     opts = function(_, opts)
       local nls = require("null-ls")
-      opts.sources = vim.list_extend(opts.sources or {}, {
+
+      opts.sources = {
+        nls.builtins.diagnostics.fish,
         -- nls.builtins.code_actions.proselint.with({ filetypes = { "tex", "markdown", "typst" } }),
         -- nls.builtins.code_actions.textlint,
         nls.builtins.completion.spell.with({
@@ -23,7 +26,7 @@ return {
         -- nls.builtins.diagnostics.vale.with({ filetypes = { "tex", "markdown", "asciidoc", "typst" } }),
         nls.builtins.hover.dictionary,
         nls.builtins.hover.printenv,
-      })
+      }
     end,
   },
 }

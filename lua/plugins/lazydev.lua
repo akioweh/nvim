6b6c933a2900a8362992462dca105eba29618999
akioweh <0@akioweh.com>
2026-02-01 22:34:41 +0000

    add luals busted and luassert type stubs via lazydev

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -7,11 +7,15 @@ return {
   {
     "folke/lazydev.nvim",
     optional = true,
-    ---@type lazydev.Config
-    opts = {
-      library = {
+    ---@param opts lazydev.Config
+    opts = function(_, opts)
+      vim.list_extend(opts.library, {
         { path = "lazy.nvim", words = { "LazySpec" } },
-      },
-    },
+        { path = "luassert-types/library", words = { "assert" } },
+        { path = "busted-types/library", words = { "describe" } },
+      })
+    end,
   },
+  { "LuaCATS/luassert", name = "luassert-types", lazy = true },
+  { "LuaCATS/busted", name = "busted-types", lazy = true },
 }

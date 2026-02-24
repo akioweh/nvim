08d086a34ef56112bcb3fac7d1493c4f8ec98f76
akioweh <0@akioweh.com>
2026-02-24 00:13:08 +0000

    update vimtex options for diff os

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,8 +3,12 @@ return {
   {
     "lervag/vimtex",
     config = function()
-      vim.g.vimtex_view_general_viewer = "SumatraPDF"
-      vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+      if vim.fn.has("win32") == 1 then
+        vim.g.vimtex_view_general_viewer = "SumatraPDF"
+        vim.g.vimtex_view_general_options = "-reuse-instance -forward-search @tex @line @pdf"
+      else
+        vim.g.vimtex_compiler_method = "tectonic"
+      end
 
       vim.g.vimtex_compiler_latexmk = {
         aux_dir = "../auxil", -- 'aux' is a reserved device name on Windows

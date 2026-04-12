320c921281cc646f49e6d71c10753b220cffcdc6
akioweh <0@akioweh.com>
2026-04-12 05:45:43 +0100

    reformat + add comments

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -3,42 +3,24 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      -- these exist only as integration with LazyVim's
-      -- custom lspconfig setup (see LazyVim/lua/lazyvim/plugins/lsp/init.lua)
+      -- these options exist only to integrate with LazyVim's
+      -- custom lspconfig setup (see LazyVim/lua/lazyvim/plugins/lsp/init.lua).
+      -- specifically, keys in `servers` are auto-enabled (or disabled)
+      -- and also kept away from LazyVim's mason auto-install with `mason = false`.
+
       ---@type table<string, vim.lsp.Config>
       servers = {
-        clangd = {
-          mason = false,
-        },
-        hls = {
-          mason = false,
-        },
-        ty = {
-          mason = false,
-        },
-        pyright = {
-          enabled = false,
-          mason = false,
-        },
-        basedpyright = {
-          enabled = false,
-          mason = false,
-        },
-        ruff = {
-          enabled = true,
-          mason = false,
-        },
-        pylsp = {
-          enabled = false,
-          mason = false,
-        },
-        tinymist = {
-          mason = false,
-        },
-        lua_ls = {
-          mason = false,
-        },
+        clangd = { mason = false },
+        hls = { mason = false },
+        ty = { mason = false },
+        tinymist = { mason = false },
+        lua_ls = { mason = false },
+        pyright = { enabled = false, mason = false },
+        basedpyright = { enabled = false, mason = false },
+        ruff = { enabled = true, mason = false },
+        pylsp = { enabled = false, mason = false },
       },
+      -- this is passed to vim.diagnostic.config() by LazyVim
       diagnostics = {
         virtual_text = {
           prefix = "icons",

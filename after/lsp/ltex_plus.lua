7e5e19ad26940bdf67681852666f112580f53a09
akioweh <0@akioweh.com>
2026-04-26 20:17:54 +0100

    tune ltex jvm opts for memory

--- after/lsp/ltex_plus.lua
+++ after/lsp/ltex_plus.lua
@@ -1,6 +1,9 @@
 ---@type vim.lsp.Config
 return {
   settings = {
+    cmd_env = {
+      JAVA_OPTS = "-Xmx2g -XX:+UseZGC -XX:+ZGenerational -Xms32m -Xmx3g -XX:ZUncommitDelay=30",
+    },
     ltex = {
       languageToolHttpServerUri = "http://localhost:8081/",
     },

19f498188f65b999d453ca8a528d05054e972a22
akioweh <0@akioweh.com>
2026-09-13 22:00:13 +0800

    lazydev: disable if .luarc.json exists

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -9,6 +9,9 @@ return {
     optional = true,
     ---@param opts lazydev.Config
     opts = function(_, opts)
+      opts.enabled = function(root_dir) -- disable if .luarc.json exists
+        return not (vim.uv.fs_stat(root_dir .. "/.luarc.json") or vim.uv.fs_stat(root_dir .. "/.luarc.jsonc"))
+      end
       vim.list_extend(opts.library, {
         { path = "lazy.nvim", words = { "LazySpec" } },
         { path = "luassert-types/library", words = { "assert" } },
